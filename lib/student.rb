98979851c2f8fa5c50d9ff8b53204de7ea71b3a3
Kristy Peterson <kpeterson.kap@gmail.com>
2017-11-13 01:02:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,17 @@
 require_relative "../config/environment.rb"
-
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn]
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+attr_accessor :name, :id, :grade
+
+def initialize (id=nil, name, grade)
+  @id = id
+  @name = name
+  @grade = grade
+end
+
+
 
 
 end
