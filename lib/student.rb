3d4f1e06e68b25527e89896e0eb7439e3d231097
Kristy Peterson <kpeterson.kap@gmail.com>
2017-11-13 01:33:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,10 +51,10 @@ end
 def self.new_from_db(row)
   new_student = self.new  # self.new is the same as running Student.new
   new_student.id = row[0]
-  new_song.name =  row[1]
-  new_song.grade = row[2]
-  new_song  # return the newly created instance
-  
+  new_student.name =  row[1]
+  new_student.grade = row[2]
+  new_student  # return the newly created instance
+end
 
 
 end
